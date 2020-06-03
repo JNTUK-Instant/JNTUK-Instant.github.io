9ba7f28b25ad701392a822f7470c563cea3aa24c
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-06-03 10:46:18 +0530

    v1.2.69 - Slick

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,1 +1,15 @@
 console.log("Created by @Phani-Codesman and @The-UCS-Variable");
+
+
+
+
+
+
+//jQuery code below...
+$('.carousel').slick({
+    infinite: true,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
