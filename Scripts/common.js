7066d0a0c3893820a738f9ec8632863c89ce9ea8
U-C-S <ucs3721@outlook.com>
2020-12-11 20:13:05 +0530

    v1.5.1 - Removed Slick - Code Format

--- Scripts/common.js
+++ Scripts/common.js
@@ -1,20 +1,3 @@
 console.log("Created by @Phani-E650 and @U-C-S");
 
-
-//jQuery code below...
-$('.carousel').slick({
-  infinite: true,
-  fade: true,
-  arrows: false,
-  speed: 100,
-  autoplay: true,
-  autoplaySpeed: 2100
-});
-
-/*
-CREDITS:
-- slick.js (src: https://kenwheeler.github.io/slick/)
-- DISQUS
--
-
-*/
+let car = document.querySelector(".carousel");
