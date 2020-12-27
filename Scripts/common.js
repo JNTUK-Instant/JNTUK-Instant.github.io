e203ae6a257efc84d0f10246bf4b8288a9b076d0
U-C-S <ucs3721@outlook.com>
2020-12-27 17:08:18 +0530

    v1.5.2

--- Scripts/common.js
+++ Scripts/common.js
@@ -1,3 +1,10 @@
 console.log("Created by @Phani-E650 and @U-C-S");
 
-let car = document.querySelector(".carousel");
+/*
+let car = document.querySelector(".carousel").getElementsByTagName("div");
+for (let i = 0; i < car.length; i++) {
+  const element = car[i];
+  set
+  
+}
+*/
